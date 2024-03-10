ce00e0f44a02c50e2b47d0d99eb629c7d3a9ecd2
fudavd <f.van.diggelen@student.vu.nl>
2024-03-10 13:00:37 +0100

    Baseline controller

---
+++ Controllers/ControlThymio.py
@@ -0,0 +1,106 @@
+
+from typing import List
+
+import numpy
+import numpy as np
+rng = numpy.random.default_rng()
+
+class Controller(object):
+    def __init__(self, n_states, n_actions, gcn_output_dim=0):
+        self.n_input = n_states
+        self.n_output = n_actions
+        self.gcn_output_dim = gcn_output_dim
+        self.controller_type = "default"
+        self.umax_const = 0.1
+        self.wmax = 1.5708 / 2.5
+
+    @staticmethod
+    def velocity_commands(state: np.ndarray) -> np.ndarray:
+        return np.array([0])
+
+    @staticmethod
+    def geno2pheno(genotype: np.array):
+        return
+
+    @staticmethod
+    def save_geno(path: str):
+        return
+
+    @staticmethod
+    def load_geno(path: str):
+        return
+
+class NumpyNetwork:
+    def __init__(self, n_input, n_hidden, n_output, reservoir=True):
+        self.reservoir = reservoir
+        self.n_con1 = n_input * n_hidden
+        self.n_con2 = n_hidden * n_output
+        self.lin1 = np.random.uniform(-1, 1, (n_hidden, n_input))
+        if reservoir:
+            self.lin2 = np.random.uniform(-1, 1, (n_hidden, n_input))
+        self.output = np.random.uniform(-1, 1, (n_output, n_hidden))
+
+    def set_weights(self, weights: np.array):
+        """
+        Set the weights of the Neural Network controller
+        """
+        if self.reservoir:
+            assert len(weights) == self.n_con2, f"Got {len(weights)} but expected {self.n_con2}"
+            weight_matrix = weights[-self.n_con2:].reshape(self.output.shape)
+            self.output = weight_matrix
+        else:
+            assert len(
+                weights) == self.n_con1 + self.n_con2, f"Got {len(weights)} but expected {self.n_con1 + self.n_con2}"
+            weight_matrix1 = weights[:self.n_con1].reshape(self.lin1.shape)
+            weight_matrix2 = weights[-self.n_con2:].reshape(self.output.shape)
+            self.lin1 = weight_matrix1
+            self.output = weight_matrix2
+
+    def forward(self, state: numpy.array):
+        # hid_l = np.maximum(np.dot(self.lin1, state)*0.01, np.dot(self.lin1, state))
+        hid_l = np.log(1 + np.exp(np.dot(self.lin1, state)))
+        if self.reservoir:
+            hid_l = np.log(1 + np.exp(np.dot(self.lin2, state)))
+        output_l = 1 / (1 + np.exp(-np.dot(self.output, hid_l)))
+        output_l[1] = output_l[1] * 2 - 1
+        return output_l
+
+
+class NNController(Controller):
+    def __init__(self, n_states, n_actions):
+        super().__init__(n_states, n_actions)
+        self.controller_type = "NN"
+        self.model = NumpyNetwork(n_states, n_states, n_actions)
+
+    def geno2pheno(self, genotype: np.array):
+        self.model.set_weights(genotype)
+
+    def map_state(self, min_from, max_from, min_to, max_to, state_portion):
+        return min_to + np.multiply((max_to - min_to), np.divide((state_portion - min_from), (max_from - min_from)))
+
+    def velocity_commands(self, state: np.ndarray) -> np.ndarray:
+        """
+        Given a state, give an appropriate action
+
+        :param <np.array> state: A single observation of the current state, dimension is (state_dim)
+        :return: <np.array> action: A vector of motor inputs
+        """
+
+        assert (len(state) == self.n_input), "State does not correspond with expected input size"
+        state[:4] = self.map_state(0, 2, -1, 1, state[:4])
+        state[4:8] = self.map_state(-np.pi, np.pi, -1, 1, state[4:8])  # Assumed distance sensing range is 2.0 meters. If not, check!
+        state[-1] = self.map_state(0, 255.0, -1, 1, state[-1])  # Gradient value, [0, 255]
+
+        action = self.model.forward(state)
+        control_input = action * np.array([self.umax_const, self.wmax])
+        return control_input
+
+    def save_geno(self, path: str):
+        if self.model.reservoir:
+            np.save(path + "/reservoir", [self.model.lin1, self.model.lin2, self.model.output], allow_pickle=True)
+
+    def load_geno(self, path: str):
+        if self.model.reservoir:
+            self.model.lin1, self.model.lin2, self.model.output = np.load(path + "/reservoir.npy", allow_pickle=True)
+
+
---
+++ thymio_baseline.py
@@ -0,0 +1,167 @@
+import os
+import sys
+import signal
+import time
+import threading
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+import numpy as np
+from tdmclient import ClientAsync
+from Controllers.ControlThymio import NNController
+
+
+uri = 'usb://0'
+global pos_xs, pos_ys, pos_hs, q1dist, q2dist, q3dist, q4dist, q1h, q2h, q3h, q4h, light_int, log_quadrant_distance, log_neg_rel_heading
+pos_xs = np.array([])
+pos_ys = np.array([])
+log_quadrant_distance = np.array([])
+log_neg_rel_heading = np.array([])
+pos_hs = 0.0
+q1dist = 99
+q2dist = 99
+q3dist = 99
+q4dist = 99
+q1h = 0
+q2h = 0
+q3h = 0
+q4h = 0
+
+# Initial values for u and w
+u = 0
+w = 0
+start_time = 0
+
+
+def activate_high_level_commander(cf):
+    cf.param.set_value('commander.enHighLevel', '1')
+
+
+def reset_estimator(cf):
+    cf.param.set_value('kalman.resetEstimation', '1')
+    time.sleep(0.1)
+    cf.param.set_value('kalman.resetEstimation', '0')
+    time.sleep(1.0)
+    cf.param.set_value('fmodes.if_takeoff', '1')
+
+
+def log_cf(scf):
+    global pos_xs, pos_ys, pos_hs, q1dist, q2dist, q3dist, q4dist, q1h, q2h, q3h, q4h, light_int, log_quadrant_distance, log_neg_rel_heading
+    log_config = LogConfig(name='Light values', period_in_ms=50)
+    log_config.add_variable('stateEstimate.x', 'float')
+    log_config.add_variable('stateEstimate.y', 'float')
+    log_config.add_variable('stateEstimate.yaw', 'float')
+
+    log_config.add_variable('synthLog.q1dist', 'uint8_t')
+    log_config.add_variable('synthLog.q2dist', 'uint8_t')
+    log_config.add_variable('synthLog.q3dist', 'uint8_t')
+    log_config.add_variable('synthLog.q4dist', 'uint8_t')
+    log_config.add_variable('synthLog.light_intensity')
+    #
+    log_config.add_variable('synthLog.q1h', 'uint8_t')
+    log_config.add_variable('synthLog.q2h', 'uint8_t')
+    log_config.add_variable('synthLog.q3h', 'uint8_t')
+    log_config.add_variable('synthLog.q4h', 'uint8_t')
+
+    with SyncLogger(scf, log_config) as logger:
+        for log_entry in logger:
+            data = log_entry[1]
+
+            pos_xs = data['stateEstimate.x']
+            pos_ys = data['stateEstimate.y']
+            pos_hs = data['stateEstimate.yaw'] * 0.0174532
+            q1dist = data['synthLog.q1dist'] * 2 / 255
+            q2dist = data['synthLog.q2dist'] * 2 / 255
+            q3dist = data['synthLog.q3dist'] * 2 / 255
+            q4dist = data['synthLog.q4dist'] * 2 / 255
+            light_int = data['synthLog.light_intensity']
+            log_quadrant_distance = np.append(log_quadrant_distance, q4dist)
+            q1h = data['synthLog.q1h'] / (255 / (3.141592*2))
+            q2h = data['synthLog.q2h'] / (255 / (3.141592*2))
+            q3h = data['synthLog.q3h'] / (255 / (3.141592*2))
+            q4h = data['synthLog.q4h'] / (255 / (3.141592*2))
+            log_neg_rel_heading = np.append(log_neg_rel_heading, q4h)
+
+
+if __name__ == '__main__':
+    cflib.crtp.init_drivers()
+    i = 0
+
+    u_max = 0.08
+    w_max = 1.5708/2.5
+    constant = 325 * (0.021 / 0.1)
+
+    ## Load Controller
+    experiment_folder = "./results/sim/Baseline/0"
+    controller = NNController(9, 2)
+    controller.load_geno(experiment_folder)
+    x_best = np.load(f"{experiment_folder}/x_best.npy")
+    genotype = x_best[99]
+    controller.geno2pheno(genotype)
+
+    with ClientAsync() as client:
+        async def change_node_var():
+            with await client.lock() as node:
+                await node.watch(variables=True)
+                await node.set_variables(targets_g)
+                # node.add_variables_changed_listener(on_variables_changed)
+
+
+        def call_program():
+            client.run_async_program(change_node_var)
+
+
+        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+            cf = scf.cf
+            thread_1 = threading.Thread(target=log_cf, args=([scf]))
+            thread_1.start()
+            reset_estimator(cf)
+            draw_angle = 1.5708/2
+
+            time_last = time.time()
+            u = 0.0
+            w = 0.0
+            left = 0
+            right = 0
+            try:
+                while True:
+                    if time.time() - time_last >= 0.05:
+
+                        # bearings = np.array([1.571/2, 1.571/2 + 1.571, 1.571/2 + 1.571*2, 1.571/2 + 1.571*3])
+                        bearings = np.array([0.0, 1.5708, np.pi, -1.5708])
+                        k = 4
+                        distances = np.array([q1dist, q2dist, q3dist, q4dist])
+                        headings = np.array([q1h, q2h, q3h, q4h])
+                        own_heading = pos_hs
+
+                        # Get velocity commands
+                        state = np.hstack((distances, headings-own_heading, light_int))
+                        u, w = controller.velocity_commands(state)
+
+                        left = constant * (u - (w * 2.75 / 2) * 0.085) / 0.021
+                        right = constant * (u + (w * 2.75 / 2) * 0.085) / 0.021
+                        if np.isnan([u, w]).any():
+                            left = 0.0
+                            right = 0.0
+                            raise ValueError
+                    # else:
+                    #     u = 0.0
+                    #     w = 0.0
+                    #     left = 0
+                    #     right = 0
+                    i += 1
+                    targets_g = {"motor.left.target": [int(left)], "motor.right.target": [int(right)]}
+                    call_program()
+            except Exception as e:
+                targets_g = {"motor.left.target": [int(0)], "motor.right.target": [int(0)]}
+                call_program()
+                print("Terminated:")
+                print(e.with_traceback())
+                # np.save('./logs/log_quad_dist.npy', log_quadrant_distance)
+                # np.save('./logs/log_neg_headings.npy', log_neg_rel_heading)
+                os.system("python3 -m tdmclient run --stop")
+                print("exiting program")
+                sys.exit()
+
