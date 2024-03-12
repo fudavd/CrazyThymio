99dec0d54dc6cfff7a0d849a9c54e2e8c7db5f50
fudavd <f.van.diggelen@student.vu.nl>
2024-03-12 15:12:47 +0100

    Baseline controller

--- thymio_baseline.py
+++ thymio_baseline.py
@@ -138,6 +138,7 @@ if __name__ == '__main__':
                         headings_rel = (headings-own_heading+np.pi)%(2*np.pi)-np.pi
                         headings_rel[distances==2.0] = 0
                         # distances[distances!=2.0] = np.sqrt(distances[distances!=2.0]-1.0)*2
+                        distances[distances!=2.0] = (distances[distances!=2.0]-0.3)/1.7*2
                         distances[distances==2.0] = 2.01
                         distances[distances<0] = 0
 
